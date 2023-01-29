581d84eea4071bdf02316f54516833b10f46a5ca
Sachin Jambhale <jambhalesachin39@gmail.com>
2023-01-29 13:04:35 +0530

    Adding Header in the home

--- parker/src/components/frontend/Home/Home.js
+++ parker/src/components/frontend/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <Header />
+    )
 }
 
 export default Home
